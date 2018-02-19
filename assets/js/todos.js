7cc36a1c544e7a7d8b5f3636787efd0e55e25b0f
Damián Adams <contact@mianlabs.com>
2018-02-19 16:36:12 -0400

    Implement the list item completed and remove functionality

---
+++ assets/js/todos.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$("li").click(function() {
+	$(this).toggleClass("completed");
+});
+
+$("span").click(function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
